7b5ec9ae0056e9817b7caa9ccd8ca47879974f2c
OGWJ <owenglynwilliamjenkins@gmail.com>
2021-08-29 11:01:36 +0100

    added PORT to dotenv

--- auth/router.js
+++ auth/router.js
@@ -31,14 +31,11 @@ app.put('/verify', async (req, res) => {
 
 // NOTE: Below account creation and deletion endpoints for testing purposes only
 router.post('/testaccount', async (req, res) => {
-
     const bcrypt = require('bcrypt');
     const salt = parseInt(process.env.SALT_ROUNDS);
-
     const schema = {
-        // For extra security, we COULD hash the emails.
-        // PROs: If database is breached, plain-text emails are not associated with personal user habits
-        // CONs: Much slower lookup times see ln.121 and cannot contact users via email after signup.
+        // should store emails in another or private collection database for only
+        // GDPR compliance and account recovery
         email: bcrypt.hashSync(req.body.email, salt),
         password: bcrypt.hashSync(req.body.password, salt),
         name: req.body.name,
@@ -47,29 +44,11 @@ router.post('/testaccount', async (req, res) => {
     }
 
     const resp = await Connection.collection.insertOne(schema);
-
     res.sendStatus(201);
 })
 
 router.delete('/testaccount', async (req, res) => {
-    client.connect(async err => {
-        const collection = client.db(DB_NAME).collection(COLLECTION_NAME)
-
-        // The extra security offered by hashing user emails makes lookup (e.g. for login or any fetch) infinitely more expensive
-        // This will take O(n)+b instead of O(1) time because it must traverse the list and compare each hash.
-
-        // LEGACY:
-        // const allUsers = await collection.find({}).toArray();
-
-        // for (user of allUsers) {
-        //     if (bcrypt.compareSync(req.body.email, user.email)) {
-        //         console.log('found target... removing')
-        //         await collection.deleteOne({ _id: user._id });
-        //     }
-        // }
-
-        client.close();
-    });
+    const resp = Connection.collection.deleteOne({ name: req.body.name });
     res.sendStatus(204);
 })
 
--- auth/server.js
+++ auth/server.js
@@ -1,7 +1,6 @@
 const express = require('express');
 const cors = require('cors');
 const bcrypt = require('bcrypt');
-const crypto = require('crypto');
 require('dotenv').config();
 const Connection = require('./dbConfig');
 const router = require('./router');
@@ -12,38 +11,4 @@ app.use(express.json());
 app.use('', router);
 
 Connection.open()
-    .then(app.listen(3000))
-
-async function verifyUser(userEmail) {
-    invalidateVerificationToken(userEmail);
-    updateUserVerificationStatus(userEmail);
-}
-
-function createNewVerificationToken() {
-    return crypto.randomBytes(60).toString('hex');
-}
-
-async function assertUserExists(userEmail) {
-    if (!await Connection.collection.findOne({ email: userEmail })) return false;
-    return true;
-}
-
-async function getUserVerificationToken(userEmail) {
-    return await Connection.collection.findOne({ email: userEmail }).verificationToken;
-}
-
-async function invalidateVerificationToken(userEmail) {
-    return await Connection.collection.updateOne({ email: userEmail }, { $set: { verificationToken: null } });
-}
-
-async function updateUserVerificationStatus(userEmail) {
-    return await Connection.collection.updateOne({ email: userEmail }, { $set: { isVerified: true } });
-}
-
-async function assertUserVerifed(userEmail) {
-    return await dbUserCollection.findOne({ email: userEmail }).isVerified;
-}
-
-async function updateUserVerificationToken(userEmail, newToken) {
-    return await Connection.collection.updateOne({ email: userEmail }, { $set: { verificationToken: newToken } });
-}
+    .then(app.listen(parseInt(process.env.PORT)))
